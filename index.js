9a384c18ed88fb06868911af380ab707578dfe6c
Dlayane-Mat <matjiladlayane@gmail.com>
2024-06-04 10:26:29 +0200

    updated my div back to h1

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function getCity(event){
     event.preventDefault();
     let searchFormInput = document.querySelector("#search-input");
     let cityElement = document.querySelector("#weather-city");
-    cityElement,innerHTML = searchFormInput.value;
+    cityElement.innerHTML = searchFormInput.value;
 }
 
 
