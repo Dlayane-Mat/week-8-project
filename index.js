52f57a0ec07ee2d9097baccd7633aa7858b583d3
Dlayane-Mat <matjiladlayane@gmail.com>
2024-06-04 10:30:57 +0200

    deleted my java rewriting my code

--- index.js
+++ index.js
@@ -1,10 +1,1 @@
-function getCity(event){
-    event.preventDefault();
-    let searchFormInput = document.querySelector("#search-input");
-    let cityElement = document.querySelector("#weather-city");
-    cityElement.innerHTML = searchFormInput.value;
-}
 
-
-let searchFormElement = document.querySelector("#search-form");
-searchFormElement.addEventListener("submit", getCity);
