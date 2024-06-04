57e53b510434e717fc3b6277ca357a0b494b5190
Dlayane-Mat <matjiladlayane@gmail.com>
2024-06-04 10:15:46 +0200

    ADDED JAVA ELEMENTS

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-alert("hello world");
+function getCity(event){
+    event.preventDefault();
+    let searchFormInput = document.querySelector("#search-input");
+    let cityElement = document.querySelector("#weather-city");
+    cityElement,innerHTML = searchFormInput.value;
+}
+
+
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", getCity);
